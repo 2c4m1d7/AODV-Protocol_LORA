080cd333a7358ea42e3ae01f6992eec7dd1bde9b
Alexander Schmidt <alexanderpochta@web.de>
2023-02-08 23:28:05 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,18 +12,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        byte firstNum = (byte) (18 >> 4);       // = 1
-        byte secondNum = (byte) (18 & 0x0f);    // = 2
-        byte thirdNum = (byte) (52 >> 4);       // = 3
-        byte fourthNum = (byte) (52 & 0x0f);    // = 4
-        out.println(firstNum);
-        out.println(secondNum);
-        out.println(thirdNum);
-        out.println(fourthNum);
-
-        if (args.length > 0) {
-            return;
-        }
         Connection connection = null;
         while (true) {
             if (connection == null) {
@@ -38,7 +26,7 @@ public class Main {
 
     private static Connection setConnection() {
         Connection connection;
-        var port = SerialPort.getCommPort("/dev/ttys003");
+//        var port = SerialPort.getCommPort("/dev/ttys003");
 
         var ports = Connection.getPorts();
         for (int i = 0; i < ports.length; i++) {
@@ -49,8 +37,8 @@ public class Main {
         if (num < 0 || num >= ports.length) {
             return null;
         }
-//        connection = new Connection(ports[num], null);
-        connection = new Connection(port, null);
+        connection = new Connection(ports[num], null);
+//        connection = new Connection(port, null);
         if (connection.connect()) {
             out.println("Opened port: " + connection.port().getDescriptivePortName());
             return connection;
